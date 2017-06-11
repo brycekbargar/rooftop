98cc1208f150a453e1c39447c1ed8a3dbae4f34f
bryce <brycekbargar@gmail.com>
2017-06-11 21:48:11 +0000

    Add a basic SVG drawing

--- src/Main.elm
+++ src/Main.elm
@@ -1,3 +1,11 @@
-import Html
+module Main exposing (..)
 
-main = Html.text "Hello World"
+import Html
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+
+main =
+    svg
+        [ width "120", height "120", viewBox "0 0 120 120" ]
+        [ rect [ x "10", y "10", width "100", height "100" ] [] ]
